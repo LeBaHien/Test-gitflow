32b9164f994486246c9c31fd81c10042021c06bf
LeBaHien <hien.leba.775@gmail.com>
2025-08-17 07:48:24 +0700

    feat(cb_init): Adding init function with cb_init

--- src/cbuffer.c
+++ src/cbuffer.c
@@ -19,7 +19,17 @@
 #define NOT_VALID                   0
 
 /* Public function ---------------------------------------------------- */
-void cb_init(cbuffer_t *cb, void *buf, uint32_t size);
+void cb_init(cbuffer_t *cb, void *buf, uint32_t size){
+    if (cb == NULL || buf == NULL)
+        return;
+    cb->data = (uint8_t*)buf;
+    cb->size = size;
+    cb->writer = 0;
+    cb->reader = 0;
+    cb->overflow = 0;
+    cb->active = true;
+    return;
+}
 
 void cb_clear(cbuffer_t *cb);
 
